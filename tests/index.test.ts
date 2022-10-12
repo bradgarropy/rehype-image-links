afd6378c5e925bc26770c08fbec08c5fe0532466
Brad Garropy <bgarropy@atlassian.com>
2022-10-12 13:38:14 -0500

    test skeleton.

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,20 @@
-import sayHello from "../src"
+import rehypeStringify from "rehype-stringify"
+import {unified} from "unified"
 
-test("says hello world", () => {
-    expect(sayHello()).toEqual("Hello, world!")
-})
+import {rehypeImageLinks} from "../src"
 
-test("says hello brad", () => {
-    expect(sayHello("Brad")).toEqual("Hello, Brad!")
+test("works", async () => {
+    const processor = unified().use(rehypeImageLinks).use(rehypeStringify)
+
+    const file = await processor.process(
+        // eslint-disable-next-line quotes
+        '<img src="https://example.com/photo.jpg"/>',
+    )
+
+    const html = file.toString()
+
+    expect(html).toEqual(
+        // eslint-disable-next-line quotes
+        '<a href="https://example.com/photo.jpg"><img src="https://example.com/photo.jpg"/></a>',
+    )
 })
