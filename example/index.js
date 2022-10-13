b331ddeead7cd01bdd7c787f3039f2210844b8cd
Brad Garropy <bgarropy@atlassian.com>
2022-10-13 10:53:59 -0500

    create example.

---
+++ example/index.js
@@ -0,0 +1,18 @@
+import rehypeParse from "rehype-parse"
+import rehypeStringify from "rehype-stringify"
+import {unified} from "unified"
+
+import {rehypeImageLinks} from "../dist/index.js"
+
+const processor = unified()
+    .use(rehypeParse, {fragment: true})
+    .use(rehypeImageLinks)
+    .use(rehypeStringify)
+
+const file = await processor.process(
+    // eslint-disable-next-line quotes
+    '<img src="https://example.com/photo.jpg" alt="alt text">',
+)
+
+const html = file.toString()
+console.log(html)
