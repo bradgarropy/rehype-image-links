cbd320c882fd1785c18a77885a904ac1809d934c
Brad Garropy <bradgarropy@gmail.com>
2023-12-22 22:27:17 -0600

    add tests.

--- tests/rehypeImageLinks.test.ts
+++ tests/rehypeImageLinks.test.ts
@@ -80,3 +80,45 @@ test("skips other elements", async () => {
         "<p>hello</p>",
     )
 })
+
+test("transforms src attribute", async () => {
+    const processor = unified()
+        .use(rehypeParse, {fragment: true})
+        .use(rehypeImageLinks, {
+            srcTransform: () => "test",
+        })
+        .use(rehypeStringify)
+
+    const file = await processor.process(
+        // eslint-disable-next-line quotes
+        '<img src="https://example.com/photo.jpg" alt="alt text">',
+    )
+
+    const html = file.toString()
+
+    expect(html).toEqual(
+        // eslint-disable-next-line quotes
+        '<a href="https://example.com/photo.jpg"><img src="test" alt="alt text"></a>',
+    )
+})
+
+test("transforms href attribute", async () => {
+    const processor = unified()
+        .use(rehypeParse, {fragment: true})
+        .use(rehypeImageLinks, {
+            hrefTransform: () => "test",
+        })
+        .use(rehypeStringify)
+
+    const file = await processor.process(
+        // eslint-disable-next-line quotes
+        '<img src="https://example.com/photo.jpg" alt="alt text">',
+    )
+
+    const html = file.toString()
+
+    expect(html).toEqual(
+        // eslint-disable-next-line quotes
+        '<a href="test"><img src="https://example.com/photo.jpg" alt="alt text"></a>',
+    )
+})
